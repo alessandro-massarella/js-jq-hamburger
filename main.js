47a257d3afe0ec5556d312184cc5f2c1127f58c2
alessandro <alessandro.massarella@gmail.com>
2020-11-02 17:42:31 +0100

    1.1

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // funzione per far apparire il menu
-$ ('.header-right a').click(function (){
-  $ ('.hamburger-menu').addClass('active');
+$('.header-right > a').click(function (){
+  $('.hamburger-menu').addClass('active');
 })
 
 // funzione per far scomparire
-$ ('.hamburger-menu a').click(function(){
-  $ ('.hamburger-menu').removeClass('active');
+$('.hamburger-menu > .close').click(function(){
+  $('.hamburger-menu').removeClass('active');
 })
